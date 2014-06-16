a5a685115ea73182728dc1c21baa88e8854890c3
reginald <reginald.lubin@sncf.com>
2014-06-16 12:49:05 +0200

    first commit

---
+++ TestGit1/src/lub/travail/git/first/GitTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package lub.travail.git.first;
+
+/**
+ * @author 8510502w
+ *
+ */
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git is fun");
+
+	}
+
+}
